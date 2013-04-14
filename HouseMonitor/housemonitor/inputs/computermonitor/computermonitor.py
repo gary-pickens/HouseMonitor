b43eb80721ed78e965b436ac2da405a0c6703c79
gary-pickens <gary_pickens@yahoo.com>
2013-04-14 09:04:20 -0500

    OPEN - task 19: Monitor computer usage http://github.com/gary-pickens/HouseMonitor/issues/issue/19

--- HouseMonitor/housemonitor/inputs/computermonitor/computermonitor.py
+++ HouseMonitor/housemonitor/inputs/computermonitor/computermonitor.py
@@ -11,19 +11,25 @@ from inputs.dataenvelope import DataEnvelope
 from lib.constants import Constants
 from lib.base import Base
 from lib.getdatetime import GetDateTime
+from datetime import timedelta
 
 
-class ComputerMonitor( Base, object ):
+class MonitorComputer( Base, threading.Thread ):
     '''
     classdocs
     '''
     input_queue = None
+    MAX_LINE_LENGTH = 120
+    forever = True
+    previous_time = GetDateTime()
+    previous_tx = 0
+    previous_rcv = 0
+
     files = {'serial': {'file':'/proc/tty/driver/OMAP-SERIAL',
-                        'regexp': re.compile( '''^.*OMAP UART1.*   # The device
-                                            tx:(?P<tx>\d*)\s*   # number of char transmitted
-                                            rx:(?P<rx>\d*)      # the number of chars received
-                                            .*$''',
-                                            flags=re.MULTILINE | re.VERBOSE )}}
+                         'regexp': re.compile( '''^.*OMAP\sUART1.*     # Select correct device
+                                                  tx:(?P<tx>\d*)\s*   # Get number of char xmited
+                                                  rx:(?P<rx>\d*)      # the number of chars received
+                                                 .*$''', flags=re.MULTILINE | re.VERBOSE )}}
 
     @property
     def logger_name( self ):
@@ -36,21 +42,25 @@ class ComputerMonitor( Base, object ):
             queue is the InputQueue
 
         '''
-        super( ComputerMonitor, self ).__init__()
+        super( MonitorComputer, self ).__init__()
         threading.Thread.__init__( self )
         self.input_queue = queue
 
 
     def extractData( self, file_contents ):
-        m = self.files[0].match( file_contents )
-        return m.groupdict()
+        m = self.files['serial']['regexp'].search( file_contents )
+        if m == None:
+            self.logger.debug( "Failed regex" )
+            return None
+        else:
+            return m.groupdict()
 
 
     def readFile( self, filename ):
         with open( filename, 'r' ) as f:
-            results = line = f.readline( 80 )
+            results = line = f.readline( self.MAX_LINE_LENGTH )
             while line:
-                line = f.readline( 80 )
+                line = f.readline( self.MAX_LINE_LENGTH )
                 results += line
             dt = GetDateTime()
         return results, dt
@@ -60,10 +70,27 @@ class ComputerMonitor( Base, object ):
             for key in self.files:
                 file_contents, datetime = self.readFile( self.files[key]['filename'] )
                 data = self.extractData( key, file_contents )
-                for key in data:
+                for key, value in data.iteritems():
+                    packet = {}
+                    packet[Constants.DataPacket.device] = 'OMAP UART1'
+                    packet[Constants.DataPacket.port] = key
+                    packet[Constants.DataPacket.arrival_time] = datetime
+                    packet[Constants.DataPacket.current_value] = value
+                    env = DataEnvelope( type='Computer', packet=packet )
+                    self.logger.debug( 'read data {}'.format( packet ) )
+                    self.input_queue.transmit( env, Constants.Queue.low_priority )
 
-#                     env = DataEnvelope( type='xbee', packet=packet )
-#                     self.logger.debug( 'read data {}'.format( packet ) )
-#                     self.input_queue.transmit( env, Constants.Queue.mid_priority )
-#                     if ( self.exit_flag ):
-#                             break
+                    delta = datetime - self.previous_time
+                    rate = int( value ) / delta.seconds()
+
+                    packet[Constants.DataPacket.device] = 'OMAP UART1'
+                    packet[Constants.DataPacket.port] = key + " rate"
+                    packet[Constants.DataPacket.arrival_time] = datetime
+                    packet[Constants.DataPacket.current_value] = rate
+                    env = DataEnvelope( type='Computer', packet=packet )
+                    self.logger.debug( 'read data {}'.format( packet ) )
+                    self.input_queue.transmit( env, Constants.Queue.low_priority )
+
+            self.previous_time = datetime
+            if ( self.forever == False ):
+                break
--- HouseMonitor/housemonitor/inputs/computermonitor/test/computermonitor_test.py
+++ HouseMonitor/housemonitor/inputs/computermonitor/test/computermonitor_test.py
@@ -11,7 +11,8 @@ from lib.constants import Constants
 import pprint
 from mock import Mock, patch, MagicMock
 from lib.getdatetime import GetDateTime
-from inputs.computermonitor import ComputerMonitor
+from inputs import dataenvelope
+from inputs.computermonitor.computermonitor import MonitorComputer
 
 
 class Test( unittest.TestCase ):
@@ -27,11 +28,30 @@ class Test( unittest.TestCase ):
 
     def test_logger_name( self ):
         queue = MagicMock()
-        cmp = ComputerMonitor( queue )
+        cmp = MonitorComputer( queue )
         self.assertEqual( cmp.logger_name, Constants.LogKeys.ComputerMonitor )
+
+    def test_extractData( self ):
+        queue = MagicMock()
+        cmp = MonitorComputer( queue )
+
+        counts = cmp.extractData( data )
+        self.assertIsNotNone( counts )
+        self.assertEqual( counts['tx'], '1188940' )
+        self.assertEqual( counts['rx'], '1427150' )
 
 
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.test_logger_name']
     unittest.main()
+
+data = '''
+serinfo:1.0 driver revision:
+0: uart:OMAP UART0 mmio:0x44E09000 irq:72 tx:85 rx:0 CTS|DSR
+1: uart:OMAP UART1 mmio:0x48022000 irq:73 tx:1188940 rx:1427150 RTS|CTS|DTR|DSR|CD|RI
+2: uart:OMAP UART2 mmio:0x48024000 irq:74 tx:0 rx:0 CTS|DSR
+3: uart:OMAP UART3 mmio:0x481A6000 irq:44 tx:0 rx:0 CTS|DSR
+4: uart:OMAP UART4 mmio:0x481A8000 irq:45 tx:0 rx:0 CTS|DSR
+5: uart:OMAP UART5 mmio:0x481AA000 irq:46 tx:0 rx:0 CTS|DSR
+'''
