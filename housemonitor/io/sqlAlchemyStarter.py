163d5e764445668bd290c37c1fb37befe81bcdaa
Gary Pickens <gary_pickens@yahoo.com>
2013-10-16 13:48:34 -0500

    Trying to get sqlalchemy working.

---
+++ housemonitor/io/sqlAlchemyStarter.py
@@ -0,0 +1,15 @@
+'''
+Created on Oct 16, 2013
+
+@author: gary
+'''
+
+class MyClass( object ):
+    '''
+    classdocs
+    '''
+
+    def __init__( self, params ):
+        '''
+        Constructor
+        '''
---
+++ housemonitor/io/sqlalchemy/data.py
@@ -0,0 +1,15 @@
+'''
+Created on Oct 16, 2013
+
+@author: gary
+'''
+
+class Data( object ):
+    '''
+    classdocs
+    '''
+
+    def __init__( self, ):
+        '''
+        Constructor
+        '''
